b320ee34da2c66dcd3d659d0d10b125a21ed6cc9
muhsin <muhsin7ak@gmail.com>
2023-11-19 16:26:30 +0530

    init vite + chakra

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Box, Text } from "@chakra-ui/layout";
+
+function App() {
+  return (
+    <>
+      <Box bgColor="teal">
+        <Text>Hello World!</Text>
+      </Box>
+    </>
+  );
+}
+
+export default App;
